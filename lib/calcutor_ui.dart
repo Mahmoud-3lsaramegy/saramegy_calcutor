c4a9079c3443ddd461dc13f9688fa1503c79ead3
git config --global user.name Mahmoud-3lsaramegy <elsaramegymahmoud404@gmail.com>
2021-11-28 10:43:01 +0200

    return button value

--- lib/calcutor_ui.dart
+++ lib/calcutor_ui.dart
@@ -1,8 +1,7 @@
+import 'dart:ffi';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
-import 'ui_function.dart';
 
 class calcutor_ui_home extends StatefulWidget {
   const calcutor_ui_home({Key? key}) : super(key: key);
@@ -12,9 +11,76 @@ class calcutor_ui_home extends StatefulWidget {
 }
 
 class _calcutor_ui_homeState extends State<calcutor_ui_home> {
-  String _text1 = '000x000';
-  String _text2 = '000000';
-  String _text3 = '0';
+  String num = "";
+  String num1 = '';
+  String num2 = '';
+  String result = '';
+  String fnialresult = "";
+  String text = '';
+  String sum() {
+    text = num1;
+    setState(() {
+      text = "";
+    });
+    text = num2;
+    setState(() {
+      text = "";
+    });
+    fnialresult = num1 + num2;
+    return fnialresult;
+  }
+
+  void clacutor(String Btext) {
+    if (Btext == "C" || Btext == "AC") {
+      setState(() {
+        num = "";
+        num1 = "";
+        num2 = "";
+        result = "";
+        fnialresult = "";
+        text = "";
+      });
+    } else if (Btext == "+" ||
+        Btext == "-" ||
+        Btext == "X" ||
+        Btext == "/" ||
+        Btext == "=" ||
+        Btext == "%" ||
+        Btext == "+/-") {
+      //do
+    } else {
+      result = result + Btext;
+      fnialresult = result;
+      setState(() {
+        text = fnialresult;
+      });
+    }
+  }
+
+  Widget button(btext, Color btextcolor) {
+    return Container(
+      width: 90,
+      height: 90,
+      padding: EdgeInsets.all(10),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Color(0xff272b33)),
+        ),
+        onPressed: () {
+          clacutor(btext);
+        },
+        child: Center(
+          child: Text(
+            btext,
+            style: TextStyle(
+              fontSize: 25,
+              color: btextcolor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +98,7 @@ class _calcutor_ui_homeState extends State<calcutor_ui_home> {
                   alignment: Alignment.centerRight,
                   width: double.infinity,
                   child: Text(
-                    _text1,
+                    "",
                     maxLines: 1,
                     textAlign: TextAlign.right,
                     style: TextStyle(
@@ -46,7 +112,7 @@ class _calcutor_ui_homeState extends State<calcutor_ui_home> {
                   padding: EdgeInsets.only(bottom: 135),
                   width: double.infinity,
                   child: Text(
-                    _text2,
+                    text,
                     maxLines: 1,
                     textAlign: TextAlign.right,
                     style: TextStyle(
@@ -69,46 +135,94 @@ class _calcutor_ui_homeState extends State<calcutor_ui_home> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    button("AC", Color(0xff26e1c0), () {}),
-                    button("+/-", Color(0xff26e1c0), () {}),
-                    button("%", Color(0xff26e1c0), () {}),
-                    button("/", Color(0xffdc6162), () {}),
+                    button(
+                      "AC",
+                      Color(0xff26e1c0),
+                    ),
+                    button(
+                      "+/-",
+                      Color(0xff26e1c0),
+                    ),
+                    button(
+                      "%",
+                      Color(0xff26e1c0),
+                    ),
+                    button(
+                      "/",
+                      Color(0xffdc6162),
+                    ),
                   ],
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    button("7", Color(0xfffafafa), () {}),
-                    button("8", Color(0xfffafafa), () {}),
-                    button("9", Color(0xfffafafa), () {}),
-                    button("X", Color(0xffec6666), () {}),
+                    button("7", Color(0xfffafafa)),
+                    button("8", Color(0xfffafafa)),
+                    button("9", Color(0xfffafafa)),
+                    button("X", Color(0xffec6666)),
                   ],
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    button("4", Color(0xfffafafa), () {}),
-                    button("5", Color(0xfffafafa), () {}),
-                    button("6", Color(0xfffafafa), () {}),
-                    button("-", Color(0xffec6666), () {}),
+                    button(
+                      "4",
+                      Color(0xfffafafa),
+                    ),
+                    button(
+                      "5",
+                      Color(0xfffafafa),
+                    ),
+                    button(
+                      "6",
+                      Color(0xfffafafa),
+                    ),
+                    button(
+                      "-",
+                      Color(0xffec6666),
+                    ),
                   ],
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    button("1", Color(0xfffafafa), () {}),
-                    button("2", Color(0xfffafafa), () {}),
-                    button("3", Color(0xfffafafa), () {}),
-                    button("+", Color(0xffec6666), () {}),
+                    button(
+                      "1",
+                      Color(0xfffafafa),
+                    ),
+                    button(
+                      "2",
+                      Color(0xfffafafa),
+                    ),
+                    button(
+                      "3",
+                      Color(0xfffafafa),
+                    ),
+                    button(
+                      "+",
+                      Color(0xffec6666),
+                    ),
                   ],
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    button("C", Color(0xfffafafa), () {}),
-                    button("0", Color(0xfffafafa), () {}),
-                    button(".", Color(0xfffafafa), () {}),
-                    button("=", Color(0xffec6666), () {}),
+                    button(
+                      "C",
+                      Color(0xfffafafa),
+                    ),
+                    button(
+                      "0",
+                      Color(0xfffafafa),
+                    ),
+                    button(
+                      ".",
+                      Color(0xfffafafa),
+                    ),
+                    button(
+                      "=",
+                      Color(0xffec6666),
+                    ),
                   ],
                 )
               ],
